9c8faa3c32699762062232e4a22d3872d936c1e5
Michael Fisher <michael@thefisherhome.net>
2015-05-13 11:04:56 -0500

    Added PDF download and Section 1 & 2 basic content.

--- content/info.php
+++ content/info.php
@@ -2,23 +2,23 @@
 		
 	<div id="info-1" class="col-md-4">
 		<div class="info-content">
-			<h2>Section 1</h2>
+			<h2>Learn More</h2>
 			<p>
-				This is the information for Section 1. It could be the who, what, why, or how section. Maybe it would have a picture at the top, maybe not. I'd suggest using it to let potential customers learn about who you are and what you do.
+				Take a moment to read through our brochure. You'll learn just a few of the many reasons why concrete countertops may be exactly what you've been looking for.
 			</p>
 			<div class="info-button">
-				<button class="btn btn-primary">Click Me!</button>
+				<a href="/dl/TopFormsBrochure.pdf" target="_blank" class="btn btn-primary">Download Brochure</a>
 			</div>
 		</div>
 	</div>
 	<div id="info-2" class="col-md-4">
 		<div class="info-content">
-			<h2>Section 2</h2>
+			<h2>About Us</h2>
 			<p>
-				This is the information for Section 2. It could be the who, what, why, or how section. Maybe it would have a picture at the top, maybe not. I'd suggest using it to let potential customers learn about who you are and what you do.
+				Ted is an awesome guy, and you should definitely hire him to custom make your countertops. Also, I have never once seen him pick his nose. So, he's got that going for him, too.
 			</p>
 			<div class="info-button">
-				<button class="btn btn-primary">Click Me!</button>
+				<a class="btn btn-primary">Click Me!</a>
 			</div>
 		</div>
 	</div>
@@ -29,7 +29,7 @@
 				This is the information for Section 3. It could be the who, what, why, or how section. Maybe it would have a picture at the top, maybe not. I'd suggest using it to let potential customers learn about who you are and what you do.
 			</p>
 			<div class="info-button">
-				<button class="btn btn-primary">Click Me!</button>
+				<a class="btn btn-primary">Click Me!</a>
 			</div>
 		</div>
 	</div>
